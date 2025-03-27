4a1d18441a1ea40d43d3c14f2a4a4c0308d86f89
VictorCasas-no <victor.casas@enti.cat>
2025-03-27 10:29:55 +0100

    27/03

--- AA2-Ajedrez_CasasMero/AA2-Ajedrez_CasasMero/main.cpp
+++ AA2-Ajedrez_CasasMero/AA2-Ajedrez_CasasMero/main.cpp
@@ -24,6 +24,23 @@ int main() {
 
 	do {
 
+
+		//Indicar turnos
+		std::cout << std::endl;                                                 //Finaliza línea previamente
+		std::cout << "---------------------------------" << std::endl;
+		std::cout << "Turno de Jugador " << turnoJugador << std::endl;          //Texto para mostrar el turno correspondiente
+		std::cout << "---------------------------------" << std::endl;
+
+		//Cambios de turno
+		if (turnoJugador == 1) {        //Condicional que cambia el turno del 1 al 2 cada vez que finaliza una ronda
+			turnoJugador = 2;
+		}
+		else {
+			turnoJugador = 1;           //Y si tenía turno el 2, cambia al 1
+		}
+
+
+
 		viewChessBoard(chessBoard);
 
 		Position userGetPiece, userSetPiece;
@@ -32,6 +49,40 @@ int main() {
 		userSetPiece = setPieceByUser(chessBoard, isWhiteTurn); 
 
 
+		//Imprimir lo que haya puesto el jugador
+		if (turnoJugador == 1) { 
+
+			if (chessBoard[userGetPiece.x][userGetPiece.y] == EMPTY && chessBoard[userSetPiece.x][userSetPiece.y] == EMPTY) {
+
+				chessBoard[userSetPiece.x][userSetPiece.y] = userGetPiece;                                        
+				std::cout << "You've moved to " << "X: " << userSetPiece.x << " Y: " << userSetPiece.y << std::endl;                           
+			}
+			else {  
+
+				chessBoard[userSetPiece.x][userSetPiece.y] != EMPTY; 
+
+				std::cout << "There's already a piece in that position. Try again." << std::endl;              
+				if (turnoJugador == 1) {
+					turnoJugador = 2;
+				}
+				else if (turnoJugador == 2) {
+					turnoJugador = 1;
+				}
+			}
+		}
+
+
+		for (int i = 0; i < BOARD_SIZE; i++) {                              //Crea i, e incrementa hasta el tamaño del tablero
+			for (int j = 0; j < BOARD_SIZE; j++) {                          //Crea j, e incrementa hasta el tamaño del tablero
+				if (chessBoard[i][j] = '*') {   //Condicional que dice que si no hay agua ni impacto...
+					std::cout << "Has movido a " << userSetPiece.x;                                  //Imprime ~ en vez de los números del principio (oculta los barcos)
+				}
+				else {
+					std::cout << chessBoard[i][j] << " ";                 //Si no se cumple el condicional, imprime normal
+				}
+			}
+			std::cout << std::endl;                                     //Finaliza la línea
+		}
 
 
 		bool isGettingAPiece = false;
